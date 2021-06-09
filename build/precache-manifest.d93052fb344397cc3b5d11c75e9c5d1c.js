4a417bc030ecae7303fc028fcf68a3602d9d8097
Benedictus Utama <bendwiutama@gmail.com>
2021-06-09 16:33:16 -0500

    removed build folder from gitignore

---
+++ build/precache-manifest.d93052fb344397cc3b5d11c75e9c5d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8639b88758150bf5468fa65d0985d69b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078e9c1e23bc2ce1dcd0",
+    "url": "/static/css/main.8a76a243.chunk.css"
+  },
+  {
+    "revision": "c610df7852504d30898c",
+    "url": "/static/js/2.18153815.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.18153815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078e9c1e23bc2ce1dcd0",
+    "url": "/static/js/main.722886fe.chunk.js"
+  },
+  {
+    "revision": "548134a5f2b94d7da7b9",
+    "url": "/static/js/runtime-main.05b044cc.js"
+  }
+]);
